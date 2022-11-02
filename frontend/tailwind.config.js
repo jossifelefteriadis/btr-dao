75b60add3de326b0d17a966015f497c0970fd579
Jossif Elefteriadis <jossif_@hotmail.com>
2022-11-02 08:51:29 +0100

    add temporary bg image

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        bgImage: "url('../public/assets/alley.webp')",
+      },
+    },
   },
   plugins: [],
 };
