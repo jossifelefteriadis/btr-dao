63e5ec4970b53f80c4c97abd26af899502cda039
Jossif Elefteriadis <jossif_@hotmail.com>
2022-11-03 13:55:38 +0100

    Add walelt connection

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,43 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import "@rainbow-me/rainbowkit/styles.css";
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+import Nav from "../components/nav";
+
+const { chains, provider } = configureChains(
+  [chain.goerli],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "BTR DAO",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider theme={darkTheme()} chains={chains}>
+        <Nav />
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
