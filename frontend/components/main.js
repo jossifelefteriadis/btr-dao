4c289e3fe0fb8f0d797f01dfd59fb14d19459f41
Jossif Elefteriadis <jossif_@hotmail.com>
2022-11-07 11:11:08 +0100

    Add main comptonent

---
+++ frontend/components/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <section className="w-full h-screen text-center bg-bgImage bg-cover bg-no-repeat">
+      <section className="max-w-[1240px] w-full h-full mx-auto p-2 flex flex-col justify-center items-center">
+        <h1>Welcome to the BTR DAO</h1>
+      </section>
+    </section>
+  );
+}
