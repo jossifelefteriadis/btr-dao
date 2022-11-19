0b732a80abaaeeda55e7e0132ba713e21345e306
Jossif Elefteriadis <jossif_@hotmail.com>
2022-11-19 20:54:13 +0100

    value component

---
+++ frontend/components/values.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Values() {
+  return (
+    <section class="h-96 flex items-center">
+      {/* https://www.covalenthq.com/about/careers/?jobId=Ryk1E03-txRV */}
+      <p>Our core values</p>
+      <p>Ownership</p>
+      <p>Educational</p>
+      <p>Urgency</p>
+    </section>
+  );
+}
