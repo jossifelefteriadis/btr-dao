77e735b366082cc3dbce896a44ffd6f2628dbf72
Jossif Elefteriadis <jossif_@hotmail.com>
2022-11-24 10:03:35 +0100

    Create footer comp

--- frontend/components/footer.js
+++ frontend/components/footer.js
@@ -2,10 +2,20 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <section>
-      <section>
-        <h1>The Footer</h1>
-      </section>
+    <section className="h-28 bg-neutral-100 pt-10 flex justify-center">
+        <section className="w-full sm:w-5/12 flex justify-evenly text-gray-900">
+          <p>Whitepaper</p>
+          <p> | </p>
+          <p>FAQ</p>
+          <p> | </p>
+          <p>About</p>
+          <p> | </p>
+          <p>Contact</p>
+          {/* <p> | </p>
+          <p>Terms of Service</p>
+          <p> | </p>
+          <p>Privacy Policy</p> */}
+        </section>
     </section>
   );
 }
