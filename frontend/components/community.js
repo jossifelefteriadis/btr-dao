8e482704ac3572590e48846e8f9bab805cc576e2
Jossif Elefteriadis <jossif_@hotmail.com>
2022-11-30 11:38:43 +0100

    Create community comp

---
+++ frontend/components/community.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Community() {
+  return (
+    <section className="w-full md:h-screen p-2 flex items-center py-16">
+      <section className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <h1>The Community Comp</h1>
+        <p>Link to youtube</p>
+        <p>Link to blog</p>
+        <p>Link to telegram</p>
+        <p>Resources/Guides</p>
+      </section>
+    </section>
+  );
+}
