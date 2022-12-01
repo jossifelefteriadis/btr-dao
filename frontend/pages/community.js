aa902a2484fb01f9a02f1f865e2ca947c72bc7f2
Jossif Elefteriadis <jossif_@hotmail.com>
2022-12-01 17:48:46 +0100

    Create community page

---
+++ frontend/pages/community.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import CommunityComp from "../components/community";
+
+export default function Community() {
+  return (
+    <section>
+      <Head>
+        <title>Learn</title>
+      </Head>
+      <CommunityComp />
+    </section>
+  );
+}
