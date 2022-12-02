31b8843f2bfcc28e65e2b6f24ee87d80456c9cbe
Jossif Elefteriadis <jossif_@hotmail.com>
2022-12-02 21:03:57 +0100

    Remove head section

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -4,9 +4,6 @@ import AboutComp from "../components/about";
 export default function About() {
   return (
     <section>
-      <Head>
-        <title>BTR DAO</title>
-      </Head>
       <AboutComp />
     </section>
   );
