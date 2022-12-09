06a2fea9a874bcf1acfc77b5002589bc90d8ac43
Jossif Elefteriadis <jossif_@hotmail.com>
2022-12-09 12:34:51 +0100

    Add about sample text

--- frontend/components/about.js
+++ frontend/components/about.js
@@ -1,11 +1,37 @@
 import React from "react";
+import VideoTestimonial from "./videoTestimonial";
 
 export default function About() {
   return (
-    <section className="w-full md:h-screen p-2 flex items-center py-16">
-      <section className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
-        <h1>The About</h1>
+    <section className="w-full p-2 flex flex-col items-center pt-28 py-16">
+      <h1>About</h1>
+      <section className="max-w-[1240px] m-auto md:grid grid-cols-2 gap-8 items-center mt-14 mb-20">
+        <section>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </section>
+        <section>
+          <p>some text</p>
+        </section>
       </section>
+      <VideoTestimonial />
     </section>
   );
 }
