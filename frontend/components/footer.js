57033a8f12b18bb02e32221db619b3344c5204b0
Jossif Elefteriadis <jossif_@hotmail.com>
2022-12-13 20:56:54 +0100

    Add links to footer

--- frontend/components/footer.js
+++ frontend/components/footer.js
@@ -1,21 +1,30 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <section className="h-28 bg-neutral-100 pt-10 flex justify-center">
-        <section className="w-full sm:w-5/12 flex justify-evenly text-gray-900">
-          <p>Whitepaper</p>
-          <p> | </p>
-          <p>FAQ</p>
-          <p> | </p>
-          <p>About</p>
-          <p> | </p>
-          <p>Contact</p>
-          {/* <p> | </p>
+      <section className="w-full sm:w-5/12 flex justify-evenly text-gray-900">
+        <p>
+          <Link href="/">Whitepaper</Link>
+        </p>
+        <p> | </p>
+        <p>
+          <Link href="/">FAQ</Link>
+        </p>
+        <p> | </p>
+        <p>
+          <Link href="/about">About</Link>
+        </p>
+        <p> | </p>
+        <p>
+          <Link href="/">Contact</Link>
+        </p>
+        {/* <p> | </p>
           <p>Terms of Service</p>
           <p> | </p>
           <p>Privacy Policy</p> */}
-        </section>
+      </section>
     </section>
   );
 }
