30c6f6007cf825a7fee76fbc789d1d870fb6c6df
Jossif Elefteriadis <jossif_@hotmail.com>
2022-12-16 16:22:46 +0100

    Add footer comp to about

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -1,9 +1,11 @@
 import AboutComp from "../components/about";
+import Footer from "../components/footer";
 
 export default function About() {
   return (
     <section>
       <AboutComp />
+      <Footer />
     </section>
   );
 }
