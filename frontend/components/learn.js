97cb4ec42cfcb1469fe74fcdbfc02073357b9bd5
Jossif Elefteriadis <jossif_@hotmail.com>
2022-12-29 18:46:00 +0100

    Add text to learn comp

--- frontend/components/learn.js
+++ frontend/components/learn.js
@@ -2,10 +2,18 @@ import React from "react";
 
 export default function Learn() {
   return (
-    <section className="w-full md:h-screen p-2 flex items-center py-16">
-      <section className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
-        <h1>The Learn Comp</h1>
-        <p>Link to whitepaper and FAQ</p>
+    <section className="w-full md:h-screen p-2 flex flex-col items-center py-16">
+      <section className="max-w-[1040px] m-auto text-xl bg-neutral-100">
+        <p>
+          We're glad you found this section and want to learn more about the BTR
+          DAO.
+        </p>
+        <br />
+        <p>
+          We have tried to make it as easy as possible to understand what this
+          community is all about and have therefore created a whitepaper and an
+          FAQ section that we want to share with you below.
+        </p>
       </section>
     </section>
   );
