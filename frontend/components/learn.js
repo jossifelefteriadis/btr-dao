5670b3fb60fb6e7bc98a9231c4a8b64753e6477f
Jossif Elefteriadis <jossif_@hotmail.com>
2022-12-30 23:24:21 +0100

    Add styling to learn comp

--- frontend/components/learn.js
+++ frontend/components/learn.js
@@ -1,18 +1,21 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Learn() {
   return (
-    <section className="w-full md:h-screen p-2 flex flex-col items-center py-16">
+    <section className="w-full md:h-screen p-2 flex flex-col items-center py-28">
       <section className="max-w-[1040px] m-auto text-xl">
         <p>
-          We're glad you found this section and want to learn more about the BTR
-          DAO.
+          We&apos;re glad you found this section and want to learn more about
+          the BTR DAO.
         </p>
         <br />
         <p>
           We have tried to make it as easy as possible to understand what this
-          community is all about and have therefore created a whitepaper and an
-          FAQ section that we want to share with you below.
+          community is all about and have therefore created a{" "}
+          <span className="text-pink-600">whitepaper</span> and an{" "}
+          <span className="text-pink-600">FAQ</span> section that we want to
+          share with you below.
         </p>
       </section>
     </section>
