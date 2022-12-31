bbacab454ccdbf55df88aca74dee2c0270312e74
Jossif Elefteriadis <jossif_@hotmail.com>
2022-12-31 10:42:21 +0100

    Add FAQ section

--- frontend/components/learn.js
+++ frontend/components/learn.js
@@ -17,6 +17,235 @@ export default function Learn() {
           <span className="text-pink-600">FAQ</span> section that we want to
           share with you below.
         </p>
+        <section className="bg-white dark:bg-gray-900 mt-8">
+          <section className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+            <h2 className="mb-8 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+              Frequently asked questions
+            </h2>
+            <section className="grid pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2">
+              <section>
+                <section className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <svg
+                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    What is the purpose of the BTR DAO?
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    The purpose of the DAO is to allow members of the BTR
+                    Investment Club to make a vote on the decisions in regards
+                    to the club.
+                  </p>
+                </section>
+                <section className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <svg
+                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    How will the DAO be governed?
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    It will be governed by the community of the BTR investors
+                    and the creators (including Jossif, the Lead Developer,
+                    Sparks Flames and Brando) will not be allowed to vote due
+                    interests of conflict and possible manipulation. They, the
+                    community, will have full control over the voting and
+                    decision-making process. Therefore making the DAO fully
+                    decentralized.
+                  </p>
+                </section>
+                <section className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <svg
+                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    How does support work?
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    We&apos;re aware of the importance of well qualified
+                    support, that is why we decided that support will be
+                    provided by the members and by the creators.
+                  </p>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    Feel free to{" "}
+                    <a
+                      href="#"
+                      className="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      contact us
+                    </a>{" "}
+                    and we&apos;ll help you out as soon as we can.
+                  </p>
+                </section>
+                <section className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <svg
+                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    What project will the DAO targeted as investment?
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    The investment will consist of projects such as Gala nodes,
+                    Thor nodes, etc. Blockchain projects that bring a passive
+                    income to the community while supporting the blockchain of
+                    that particular company. Of course, it will depend on the
+                    decision made by the BTR investors.
+                  </p>
+                </section>
+                <section className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <svg
+                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    Will there be voting on projects?
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    Yes there will be voting as stated previously, all decisions
+                    are to be made by the BTR investors only.
+                  </p>
+                </section>
+              </section>
+              <section>
+                <section className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <svg
+                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    How will the DAO investor communicate?
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    All communication is to be made via the Telegram group that
+                    was created for Patreon members that pay a monthly fee.
+                  </p>
+                </section>
+                <section className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <svg
+                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    Will there be regular meetings?
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    There will be if needed by Brando Murphy to address issues
+                    or make announcements.
+                  </p>
+                </section>
+                <section className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <svg
+                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    How many people are committed to the DAO at this Present
+                    time?
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    According to the SEC regulations, only 100 people may commit
+                    to an investment club.
+                  </p>
+                </section>
+                <section className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <svg
+                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    What positions do our founding partners have in the
+                    development of the DAO?
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    Jossif handles all the development such as the BTR DAO site,
+                    NFT creation, Smart Contracts, etc. Sparks Flames handles
+                    admin which includes collecting and distributing information
+                    to the members of the investment. Brando provides the vision
+                    and overlooks the full operation.
+                  </p>
+                </section>
+              </section>
+            </section>
+          </section>
+        </section>
       </section>
     </section>
   );
