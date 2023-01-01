0ef11107e187844d4b7fa32170914964f208dde0
Jossif Elefteriadis <jossif_@hotmail.com>
2023-01-01 20:32:43 +0100

    Refactor

--- frontend/components/learn.js
+++ frontend/components/learn.js
@@ -86,32 +86,6 @@ export default function Learn() {
                         clip-rule="evenodd"
                       ></path>
                     </svg>
-                    How does support work?
-                  </h3>
-                  <p className="text-gray-500 dark:text-gray-400">
-                    We&apos;re aware of the importance of well qualified
-                    support, that is why we decided that support will be
-                    provided by the members and by the creators.
-                  </p>
-                  <p className="text-gray-500 dark:text-gray-400">
-                    Feel free to use the Telegram group for all questions, or
-                    the contact form of this website.
-                  </p>
-                </section>
-                <section className="mb-10">
-                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
-                    <svg
-                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
                     What project will the DAO targeted as investment?
                   </h3>
                   <p className="text-gray-500 dark:text-gray-400">
@@ -164,6 +138,32 @@ export default function Learn() {
                   <p className="text-gray-500 dark:text-gray-400">
                     All communication is to be made via the Telegram group that
                     was created for Patreon members that pay a monthly fee.
+                  </p>
+                </section>
+                <section className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    <svg
+                      className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    How does support work?
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    We&apos;re aware of the importance of well qualified
+                    support, that is why we decided that support will be
+                    provided by the members and by the creators.
+                  </p>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    Feel free to use the Telegram group for all questions, or
+                    the contact form of this website.
                   </p>
                 </section>
                 <section className="mb-10">
