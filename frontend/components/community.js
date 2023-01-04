1ff6f9ed4477ed26f02b0068dd44f517348ce872
Jossif Elefteriadis <jossif_@hotmail.com>
2023-01-04 19:49:23 +0100

    First draft community

--- frontend/components/community.js
+++ frontend/components/community.js
@@ -1,14 +1,30 @@
 import React from "react";
+import Image from "next/image";
+import arrow from "../public/assets/icons/next.png";
 
 export default function Community() {
   return (
-    <section className="w-full md:h-screen p-2 flex items-center py-16">
-      <section className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
-        <h1>The Community Comp</h1>
-        <p>Link to youtube</p>
-        <p>Link to blog</p>
-        <p>Link to telegram</p>
-        <p>Resources/Guides</p>
+    <section className="w-full text-center bg-white pt-40">
+      <section className="max-w-[1240px] h-96 mx-auto p-2 flex flex-col justify-center items-center bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+        <h1>Join the BTR Community</h1>
+      </section>
+      <section className="max-w-[1240px] m-auto mt-10 pb-20 md:grid grid-cols-12 gap-8">
+        <section className="h-24 flex items-center justify-between p-6 col-start-2 col-span-4 border-2 rounded border-gray-900 text-gray-900">
+          <p>Link to youtube</p>
+          <Image src={arrow} alt="arrow" width="25" height="25" />
+        </section>
+        <section className="h-24 flex items-center justify-between p-6 col-start-8 col-span-4 border-2 rounded border-gray-900 text-gray-900">
+          <p>Link to blog</p>
+          <Image src={arrow} alt="arrow" width="25" height="25" />
+        </section>
+        <section className="h-24 flex items-center justify-between p-6 col-start-2 col-span-4 border-2 rounded border-gray-900 text-gray-900">
+          <p>Link to telegram</p>
+          <Image src={arrow} alt="arrow" width="25" height="25" />
+        </section>
+        <section className="h-24 flex items-center justify-between p-6 col-start-8 col-span-4 border-2 rounded border-gray-900 text-gray-900">
+          <p>Resources/Guides</p>
+          <Image src={arrow} alt="arrow" width="25" height="25" />
+        </section>
       </section>
     </section>
   );
