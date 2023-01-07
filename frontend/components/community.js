6c5a19a29c0e6db485a2808c93eb2d44c7debd1a
Jossif Elefteriadis <jossif_@hotmail.com>
2023-01-07 09:53:17 +0100

    Change link and styling boxes

--- frontend/components/community.js
+++ frontend/components/community.js
@@ -10,26 +10,46 @@ export default function Community() {
         <h1>Join the BTR Community</h1>
       </section>
       <section className="max-w-[1240px] m-auto mt-10 pb-20 md:grid grid-cols-12 gap-8">
-        <section className="h-24 flex items-center justify-between p-6 col-start-2 col-span-4 border-2 rounded border-gray-900 text-gray-900">
-          <p>Link to youtube</p>
-          <Image src={arrow} alt="arrow" width="25" height="25" />
-        </section>
-        <section className="h-24 flex items-center justify-between p-6 col-start-8 col-span-4 border-2 rounded border-gray-900 text-gray-900">
-          <p>Link to blog</p>
-          <Image src={arrow} alt="arrow" width="25" height="25" />
-        </section>
-        <section className="h-24 flex items-center justify-between p-6 col-start-2 col-span-4 border-2 rounded border-gray-900 text-gray-900">
-          <p>Link to telegram</p>
-          <Image src={arrow} alt="arrow" width="25" height="25" />
-        </section>
-        <section className="h-24 flex items-center justify-between p-6 col-start-8 col-span-4 border-2 rounded border-gray-900 text-gray-900">
-          <p>
-            <Link href="https://biggerthanrace.com/" target="_blank">
-              Link to website
-            </Link>
-          </p>
-          <Image src={arrow} alt="arrow" width="25" height="25" />
-        </section>
+        <Link
+          href="https://www.youtube.com/c/BiggerThanRace"
+          target="_blank"
+          className="h-24 flex items-center p-6 col-start-2 col-span-4 border-2 rounded border-gray-700 text-gray-900 hover:shadow-lg"
+        >
+          <section className="w-full flex items-center justify-between">
+            <p>Link to youtube</p>
+            <Image src={arrow} alt="arrow" width="25" height="25" />
+          </section>
+        </Link>
+        <Link
+          href="https://www.youtube.com/c/BiggerThanRace"
+          target="_blank"
+          className="h-24 flex items-center p-6 col-start-8 col-span-4 border-2 rounded border-gray-700 text-gray-900 hover:shadow-lg"
+        >
+          <section className="w-full flex items-center justify-between">
+            <p>Link to blog</p>
+            <Image src={arrow} alt="arrow" width="25" height="25" />
+          </section>
+        </Link>
+        <Link
+          href="https://www.youtube.com/c/BiggerThanRace"
+          target="_blank"
+          className="h-24 flex items-center p-6 col-start-2 col-span-4 border-2 rounded border-gray-700 text-gray-900 hover:shadow-lg"
+        >
+          <section className="w-full flex items-center justify-between">
+            <p>Link to telegram</p>
+            <Image src={arrow} alt="arrow" width="25" height="25" />
+          </section>
+        </Link>
+        <Link
+          href="https://biggerthanrace.com/"
+          target="_blank"
+          className="h-24 flex items-center p-6 col-start-8 col-span-4 border-2 rounded border-gray-700 text-gray-900 hover:bg-neutral-50"
+        >
+          <section className="w-full flex items-center justify-between">
+            <p>Link to website</p>
+            <Image src={arrow} alt="arrow" width="25" height="25" />
+          </section>
+        </Link>
         <section className="h-24 flex items-center justify-between p-6 col-start-2 col-span-4 border-2 rounded border-gray-900 text-gray-900">
           <p>Resources/Guides</p>
           <Image src={arrow} alt="arrow" width="25" height="25" />
