449b3bdef684bee0bf990b715131ec6b32a221e8
Jossif Elefteriadis <jossif_@hotmail.com>
2023-01-09 21:44:39 +0100

    Add contact page

---
+++ frontend/pages/contact.js
@@ -0,0 +1,11 @@
+import ContactComp from "../components/contact";
+import Footer from "../components/footer";
+
+export default function Contact() {
+  return (
+    <section>
+      <ContactComp />
+      <Footer />
+    </section>
+  );
+}
