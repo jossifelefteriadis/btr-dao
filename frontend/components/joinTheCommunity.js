6cf839be82c0b91a0eb09ecd663f1bdd755e53ae
Jossif Elefteriadis <jossif_@hotmail.com>
2023-01-15 17:48:04 +0100

    placeholder text for join the community section

---
+++ frontend/components/joinTheCommunity.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function JoinTheCommunity() {
+  return (
+    <section className="bg-neutral-100 h-96 flex items-center ">
+      <section className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+        <p>Join the community section here</p>
+      </section>
+    </section>
+  );
+}
