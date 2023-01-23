d8d4cffb7b185801421158c8d0a01c2c1b709c99
Jossif Elefteriadis <jossif_@hotmail.com>
2023-01-23 08:34:50 +0100

    Add video testimonial

--- frontend/components/videoTestimonial.js
+++ frontend/components/videoTestimonial.js
@@ -4,6 +4,15 @@ export default function VideoTestimonial() {
   return (
     <section>
       <p>Insert video testimonial here</p>
+      <iframe
+        width="315"
+        height="560"
+        src="https://youtube.com/embed/Ftqjr9aqkZA?feature=share"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
     </section>
   );
 }
