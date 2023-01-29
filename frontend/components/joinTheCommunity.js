842b94ac7db02c13137c5bad86885f873f6da081
Jossif Elefteriadis <jossif_@hotmail.com>
2023-01-29 10:12:25 +0100

    Add links

--- frontend/components/joinTheCommunity.js
+++ frontend/components/joinTheCommunity.js
@@ -15,22 +15,46 @@ export default function JoinTheCommunity() {
         {/* <section className="max-w-[1240px] h-96 mx-auto p-2 flex flex-col justify-center items-center bg-white dark:bg-gray-900 text-gray-900 dark:text-white"> */}
         <p>Join the community section here</p>
         <section className="flex items-center justify-evenly w-4/6 pt-14">
-          <section className="flex flex-col items-center justify-evenly h-24">
-            <Image src={youtube} alt="youtube" width="35" height="35" />
-            <p>Youtube</p>
-          </section>
-          <section className="flex flex-col items-center justify-evenly h-24">
-            <Image src={blog} alt="blog" width="35" height="35" />
-            <p>Blog</p>
-          </section>
-          <section className="flex flex-col items-center justify-evenly h-24">
-            <Image src={telegram} alt="telegram" width="35" height="35" />
-            <p>Telegram</p>
-          </section>
-          <section className="flex flex-col items-center justify-evenly h-24">
-            <Image src={website} alt="website" width="35" height="35" />
-            <p>Website</p>
-          </section>
+          <Link
+            href="https://www.youtube.com/c/BiggerThanRace"
+            target="_blank"
+            // className="h-24 flex items-center p-6 col-start-2 col-span-5 border-2 rounded border-gray-700 text-gray-900 hover:shadow-lg"
+          >
+            <section className="flex flex-col items-center justify-evenly h-24">
+              <Image src={youtube} alt="youtube" width="35" height="35" />
+              <p>Youtube</p>
+            </section>
+          </Link>
+          <Link
+            href="https://biggerthanrace.com/blog"
+            target="_blank"
+            // className="h-24 flex items-center p-6 col-start-2 col-span-5 border-2 rounded border-gray-700 text-gray-900 hover:shadow-lg"
+          >
+            <section className="flex flex-col items-center justify-evenly h-24">
+              <Image src={blog} alt="blog" width="35" height="35" />
+              <p>Blog</p>
+            </section>
+          </Link>
+          <Link
+            href="https://www.patreon.com/BiggerThanRace/"
+            target="_blank"
+            // className="h-24 flex items-center p-6 col-start-2 col-span-5 border-2 rounded border-gray-700 text-gray-900 hover:shadow-lg"
+          >
+            <section className="flex flex-col items-center justify-evenly h-24">
+              <Image src={telegram} alt="telegram" width="35" height="35" />
+              <p>Telegram</p>
+            </section>
+          </Link>
+          <Link
+            href="https://biggerthanrace.com/"
+            target="_blank"
+            // className="h-24 flex items-center p-6 col-start-2 col-span-5 border-2 rounded border-gray-700 text-gray-900 hover:shadow-lg"
+          >
+            <section className="flex flex-col items-center justify-evenly h-24">
+              <Image src={website} alt="website" width="35" height="35" />
+              <p>Website</p>
+            </section>
+          </Link>
         </section>
       </section>
     </section>
