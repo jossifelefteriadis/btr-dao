7765e003a47521c52e4be77561bba633fbb7a36c
Jossif Elefteriadis <jossif_@hotmail.com>
2023-02-05 16:22:26 +0100

    Render explainer container

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,6 +4,7 @@ import Description from "../components/description";
 import Testimonial from "../components/testimonial";
 import Values from "../components/values";
 import JoinTheCommunity from "../components/joinTheCommunity";
+import Explainer from "../components/explainer";
 import Footer from "../components/footer";
 
 export default function Home() {
@@ -17,9 +18,7 @@ export default function Home() {
       <Testimonial />
       <Values />
       <JoinTheCommunity />
-      <p>Have an NFT to create proposals - visa nft bild</p>
-      <p>Have an NFT to vote on proposals - visa nft bild</p>
-      <p>OVer 51% proposals - 6 rutor där 4 är gröna</p>
+      <Explainer />
       <Footer />
     </section>
   );
