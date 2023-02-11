cec6eb4e31f79db661cd237f67f473e6c6c9000f
Jossif Elefteriadis <jossif_@hotmail.com>
2023-02-11 16:12:32 +0100

    Add vote comp

---
+++ frontend/components/vote.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Vote() {
+  return (
+    <section className="pb-20">
+      <p className="pt-4 w-2/3 text-lg text-center">Vote component</p>
+    </section>
+  );
+}
