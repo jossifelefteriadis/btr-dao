c5c18ad6d8ab78f411466996cc45742238bba874
Jossif Elefteriadis <jossif_@hotmail.com>
2023-02-13 18:27:15 +0100

    Refactor

--- frontend/components/vote.js
+++ frontend/components/vote.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Vote() {
   return (
     <section className="pb-20">
-      <p className="pt-4 w-2/3 text-lg text-center">Vote component</p>
+      <p className="pt-4 w-2/3 text-lg text-center">Vote Component</p>
     </section>
   );
 }
