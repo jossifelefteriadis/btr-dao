7d12e9553670911ef9d5edf0058f2eeb274a4f8e
Jossif Elefteriadis <jossif_@hotmail.com>
2023-03-13 08:37:10 +0100

    Add testimonial

--- frontend/components/community.js
+++ frontend/components/community.js
@@ -113,6 +113,17 @@ export default function Community() {
           </section>
         </Link>
       </section>
+      <section className="w-full flex flex-col items-center pb-20">
+        <iframe
+          width="315"
+          height="560"
+          src="https://www.youtube.com/embed/6kf5IgZC4ho"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </section>
     </section>
   );
 }
