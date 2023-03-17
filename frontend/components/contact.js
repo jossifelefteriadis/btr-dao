f4276b52078eb79c5b000f3ab975ff4e524281af
Jossif Elefteriadis <jossif_@hotmail.com>
2023-03-17 18:58:42 +0100

    Add functionality for sending emails

--- frontend/components/contact.js
+++ frontend/components/contact.js
@@ -1,9 +1,26 @@
 import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
   const SERVICE_ID = process.env.NEXT_PUBLIC_SERVICE_ID;
   const TEMPLATE_ID = process.env.NEXT_PUBLIC_TEMPLATE_ID;
   const PUBLIC_KEY = process.env.NEXT_PUBLIC_PUBLIC_KEY;
+
+  const form = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY).then(
+      (result) => {
+        console.log(`Message was sent successfully: ${result.text}`);
+      },
+      (error) => {
+        console.log(`Message was not sent successfully: ${error.text}`);
+      }
+    );
+    e.target.reset();
+  };
 
   return (
     <section className="w-full flex flex-col items-center pt-32 py-16 bg-white text-gray-900">
@@ -12,8 +29,7 @@ export default function Contact() {
         <section className="max-w-[1240px] m-auto px-2 py-16 w-full">
           <section className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
             <section className="p-4">
-              {/* <form ref={form} onSubmit={handleSubmit}> */}
-              <form>
+              <form ref={form} onSubmit={handleSubmit}>
                 <section className="grid md:grid-cols-2 gap-4 w-full py-2">
                   <section className="flex flex-col">
                     <label className="uppercase text-sm py-2">Name</label>
