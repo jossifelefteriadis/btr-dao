a217b8f72884d2e3e1b5871486b266479e4b8b73
Jossif Elefteriadis <jossif_@hotmail.com>
2023-03-18 08:43:29 +0100

    [WIP] vote component

--- frontend/components/vote.js
+++ frontend/components/vote.js
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { useAccount } from "wagmi";
+import { AlertTriangle } from "@web3uikit/icons";
 
 export default function Vote() {
+  const { isConnected } = useAccount();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (!isConnected) {
+      setIsLoggedIn(false);
+    } else {
+      setIsLoggedIn(true);
+    }
+  }, [isConnected]);
+
   return (
-    <section className="pb-20">
-      <p className="pt-4 w-2/3 text-lg text-center">Vote Component</p>
+    <section className="py-28">
+      {!isLoggedIn ? (
+        <section className="min-h-screen flex flex-col items-center mt-8">
+          <section className="w-2/6 flex justify-between items-center">
+            <AlertTriangle fontSize="80px" className="text-red-600" />
+            <section className="w-84 h-56 flex flex-col justify-center text-2xl text-center">
+              <p className="mb-1">
+                THIS IS A{" "}
+                <span className="border-b-2 border-red-600">
+                  RESTRICTED AREA
+                </span>
+              </p>
+              <p>
+                FOR <span className="text-pink-600">BTR DAO</span> MEMBERS ONLY
+              </p>
+            </section>
+            <AlertTriangle fontSize="80px" className="text-red-600" />
+          </section>
+          <section className="flex flex-col justify-evenly items-center h-20 mt-16 text-xl tracking-wide">
+            <p>CONNECT YOUR WALLET IF YOU ARE A BTR DAO MEMBER</p>
+            <p>
+              NOT A MEMBER?{" "}
+              <a
+                href="https://www.patreon.com/BiggerThanRace/"
+                target="_blank"
+                className="px-2 py-1 text-black font-semibold bg-pink-600 hover:bg-black hover:text-pink-600 cursor-pointer"
+              >
+                JOIN THE BTR COMMUNITY
+              </a>
+            </p>
+          </section>
+        </section>
+      ) : (
+        <section>
+          <p>logged in</p>
+        </section>
+      )}
     </section>
   );
 }
