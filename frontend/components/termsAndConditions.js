6be6be92d95de26bececad2aa4559d4c667db699
Jossif Elefteriadis <jossif_@hotmail.com>
2023-04-08 16:27:51 +0200

    Add Terms & Conditions component

---
+++ frontend/components/termsAndConditions.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+export default function TermsAndConditions() {
+  return (
+    <section className="w-full p-2 flex flex-col items-center pt-28 py-16">
+      <h1>Terms & Conditions</h1>
+      <section className="mt-10 px-36">
+        <p className="mb-4 font-semibold">
+          Bigger Than Race Disclaimer / Terms & Conditions of Service and
+          Investment.
+        </p>
+        <section className="text-lg leading-10">
+          <p>
+            You should always do your own research and be familiar with the
+            risks involved before investing any money
+          </p>
+          <p>
+            Crypto currency is a high risk investment - there is a chance you
+            could lose 100% of your original investment
+          </p>
+          <p>
+            If any token/project doesn't perform well and you receive a loss,
+            the responsibility is YOURS alone
+          </p>
+          <p>
+            Bigger Than Race are in no way responsible for any losses incurred
+            as a result of any investment
+          </p>
+          <p>
+            Bigger Than Race only offers investment opportunities; this in no
+            way constitutes financial advice or encouragement to invest
+          </p>
+          <p>
+            By participating in any investment offered by Bigger Than Race, you
+            confirm the following:
+          </p>
+          <section>
+            <p>
+              <span className="text-pink-600">That you</span> must complete KYC
+              (know your customer) before investing - we provide this as a free
+              service through the Presail platform - this is essential in order
+              for us to comply with anti money laundering regulations
+            </p>
+            <p>
+              <span className="text-pink-600">That you</span> are legally able
+              to participate and do NOT reside in a country/area where
+              participation is prohibited - this is YOUR responsibility to check
+              BEFORE contributing
+            </p>
+            <p>
+              <span className="text-pink-600">That you</span> know the full
+              details of the token launch, as provided
+            </p>
+            <p>
+              <span className="text-pink-600">That you</span> will invest based
+              only on your own research
+            </p>
+            <p>
+              <span className="text-pink-600">That you</span> are familiar with
+              and accept the risks of investing
+            </p>
+            <p>
+              <span className="text-pink-600">That you</span> have to send funds
+              before the sale
+            </p>
+            <p>
+              <span className="text-pink-600">That you</span> have given your
+              own wallet address in order to receive the tokens
+            </p>
+            <p>
+              <span className="text-pink-600">That no</span> investments can be
+              refunded, once made
+            </p>
+          </section>
+        </section>
+      </section>
+    </section>
+  );
+}
