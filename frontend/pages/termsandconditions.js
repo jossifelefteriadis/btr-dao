9901a649c2e10c47ad8499052ee87074b9ed89e9
Jossif Elefteriadis <jossif_@hotmail.com>
2023-04-08 16:25:44 +0200

    Add Terms and Conditions page

---
+++ frontend/pages/termsandconditions.js
@@ -0,0 +1,11 @@
+import TermsAndConditionsComp from "../components/termsAndConditions";
+import Footer from "../components/footer";
+
+export default function TermsAndConditions() {
+  return (
+    <section>
+      <TermsAndConditionsComp />
+      <Footer />
+    </section>
+  );
+}
