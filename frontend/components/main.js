afbb4c82315125747ab9c2740fa98f5348b3f308
Jossif Elefteriadis <jossif_@hotmail.com>
2023-04-13 18:17:31 +0200

    change to BTR Investment Club

--- frontend/components/main.js
+++ frontend/components/main.js
@@ -4,7 +4,7 @@ export default function Main() {
   return (
     <section className="w-full h-screen text-center bg-bgImage bg-cover bg-no-repeat">
       <section className="max-w-[1240px] w-full h-full mx-auto p-2 flex flex-col justify-center items-center">
-        <h1>Welcome to the BTR DAO</h1>
+        <h1>Welcome to the BTR Investment Club</h1>
       </section>
     </section>
   );
